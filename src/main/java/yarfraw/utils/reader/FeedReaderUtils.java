f8588632d6092d6a7954f002008ef827e458540a
zl25-drexel <zl25-drexel@fa792110-9d37-0410-8bf1-8cf09365c9f7>
2007-09-04 12:43:32 +0000

    removed printStacktrace
    git-svn-id: https://svn.code.sf.net/p/yarfraw/code/yarfraw/yarfraw@159 fa792110-9d37-0410-8bf1-8cf09365c9f7

--- src/main/java/yarfraw/utils/reader/FeedReaderUtils.java
+++ src/main/java/yarfraw/utils/reader/FeedReaderUtils.java
@@ -63,12 +63,10 @@ public class FeedReaderUtils{
       }
       catch (InterruptedException e) {
         LOG.error("Interrupted exception received", e);
-        e.printStackTrace();
         ret.add(null);
       }
       catch (ExecutionException e) {
         LOG.error("Execution exception received", e);
-        e.printStackTrace();
         ret.add(null);
       }
     }
